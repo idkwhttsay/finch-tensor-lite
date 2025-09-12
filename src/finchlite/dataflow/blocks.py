97460b28b56b2ec175ccb6e1980eb9c12cad98f4
idkwhttsay <danil.tatarinov.00@gmail.com>
2025-09-12 00:26:30 -0400

    feat: implement main logic for building CFG for loops, ifs, and assigns
    Signed-off-by: idkwhttsay <danil.tatarinov.00@gmail.com>

--- src/finchlite/dataflow/blocks.py
+++ src/finchlite/dataflow/blocks.py
@@ -1,8 +1,5 @@
 from ..finch_assembly.nodes import *
 
-# TODO: Do a system design for how to build Basic Blocks and CFG based on the AST. Understand how to handle loops, breaks, continues, function calls, etc. \
-# and how to represent them in the CFG. Also, figure out a better way of representiang the connections between blocks (probably a map will work best).
-
 class BasicBlock:
     def __init__(self, id):
         self.id = id
@@ -19,3 +16,103 @@ class BasicBlock:
 
     def __repr__(self):
         return f"BasicBlock(id={self.id}, stmts={self.statements}, succs={self.successors})"
+
+class CFGBuilderContext:
+    def __init__(self):
+        self.blocks = {}
+        self.block_counter = 0
+        self.current_block = self.new_block()
+    
+    def new_block(self):
+        bid = f"b{self.block_counter}"
+        self.block_counter += 1
+        block = BasicBlock(bid)
+        self.blocks[bid] = block
+        return block
+
+    def build(self, node: AssemblyNode):
+        return self(node)
+    
+    def __call__(self, node: AssemblyNode):
+        match node:
+            case Literal(_) | Variable(_, _) | GetAttr(_, _) | Load(_, _) | Store(_, _, _) | Length(_, _) | Slot(_, _) | Stack(_, _):
+                return
+            case Assign(Variable(name, _), val):
+                self.current_block.add_statement(("assign", name, val))
+            case Block(bodies):
+                for body in bodies:
+                    self(body)
+            case If(cond, body):
+                cond_block = self.current_block
+                cond_block.add_statement(("if_cond", cond))
+                then_block = self.new_block()
+                self.current_block = then_block
+                self(body)
+                after_block = self.new_block()
+                cond_block.add_successor(then_block.id)
+                cond_block.add_successor(after_block.id)
+                self.current_block.add_successor(after_block.id)
+                self.current_block = after_block
+            case IfElse(cond, body, else_body):
+                cond_block = self.current_block
+                cond_block.add_statement(("if_else_cond", cond))
+                then_block = self.new_block()
+                self.current_block = then_block
+                self(body)
+                after_block = self.new_block()
+                cond_block.add_successor(then_block.id)
+                cond_block.add_successor(after_block.id) # i think it's not necessary
+                self.current_block.add_successor(after_block.id)
+
+                else_block = self.new_block()
+                self.current_block = else_block
+                self(else_body)
+                self.current_block.add_successor(after_block.id)
+                self.current_block = after_block
+            case WhileLoop(cond, body):
+                cond_block = self.current_block
+                cond_block.add_statement(("while_cond", cond))
+                body_block = self.new_block()
+                after_block = self.new_block()
+                cond_block.add_successor(body_block.id)
+                cond_block.add_successor(after_block.id)
+                self.current_block = body_block
+                self(body)
+                self.current_block.add_successor(cond_block.id)
+                self.current_block = after_block
+            case ForLoop(var, start, end, body):
+                init_block = self.current_block
+                init_block.add_statement(("for_init", var, start, end))
+
+                cond_block = self.new_block()
+                init_block.add_successor(cond_block.id)
+                cond_block.add_statement(("for_init", var, start, end))
+
+                body_block = self.new_block()
+                cond_block.add_successor(body_block)
+                
+                after_block = self.new_block()
+                cond_block.add_successor(after_block)
+
+                self.current_block = body_block
+                self(body_block)
+
+                self.current_block.add_statement(("for_inc", var))
+                self.current_block.add_successor(cond_block.id)
+
+                self.current_block = after_block
+            case Return(value): # TODO: when Return is met probably return None and check if self.current_block == None, if yes go to the end of the function
+                self.current_block.add_statement(("return", value))
+                self.current_block = self.new_block()
+            case BufferLoop(buf, var, body):
+                raise NotImplementedError(node) # TODO: change when BufferLoop is implemented (what is a BufferLoop)?
+            case Function(Variable(func_name, return_t), args, body): # TODO: create a separe CFG for every function
+                ...
+            case Break(): # TODO: is it really necessary to handle break when we always proceed
+                ...
+            case Module(funcs): # TODO: handle modules with multiple functions
+                ...
+            case node:
+                raise NotImplementedError(node)
+        
+        return self.blocks
