dd65d2f00d6ffb0cb7e5c2d7dfd041aca77d018e
idkwhttsay <danil.tatarinov.00@gmail.com>
2025-09-12 19:25:13 -0400

    refac: run pre-commit and refactor
    Signed-off-by: idkwhttsay <danil.tatarinov.00@gmail.com>

--- src/finchlite/finch_assembly/cfg.py
+++ src/finchlite/finch_assembly/cfg.py
@@ -1,5 +1,31 @@
-from .nodes import *
-from typing import Dict
+from .nodes import (
+    AssemblyNode,
+    Assign,
+    Block,
+    Break,
+    BufferLoop,
+    Call,
+    ForLoop,
+    Function,
+    GetAttr,
+    If,
+    IfElse,
+    Length,
+    Literal,
+    Load,
+    Module,
+    Repack,
+    Resize,
+    Return,
+    SetAttr,
+    Slot,
+    Stack,
+    Store,
+    Unpack,
+    Variable,
+    WhileLoop,
+)
+
 
 class BasicBlock:
     def __init__(self, id):
@@ -11,20 +37,21 @@ class BasicBlock:
     def add_statement(self, statement):
         self.statements.append(statement)
 
-    def add_successor(self, succ_id: str, blocks: Dict[str, 'BasicBlock']) -> None:
+    def add_successor(self, succ_id: str, blocks: dict[str, "BasicBlock"]) -> None:
         if succ_id not in self.successors:
             self.successors.append(succ_id)
-        
+
         if self.id not in blocks[succ_id].predecessors:
             blocks[succ_id].predecessors.append(self.id)
 
     def __repr__(self):
         return f"BasicBlock(id={self.id}, stmts={self.statements}, succs={self.successors})"
-    
+
+
 class CFG:
-    def __init__(self, func_name: str): 
+    def __init__(self, func_name: str):
         # TODO: also pass argument types to allow fuctions with same names but different signatures
-        
+
         self.block_counter = 0
         self.name = func_name
         self.blocks = {}
@@ -35,7 +62,7 @@ class CFG:
 
         self.current_block = self.new_block()
         self.entry_block.add_successor(self.current_block, self.blocks)
-    
+
     def new_block(self):
         bid = f"{self.name}_{self.block_counter}"
         self.block_counter += 1
@@ -43,7 +70,8 @@ class CFG:
         self.blocks[bid] = block
         return block
 
-class CFGBuilderContext: # is it required to have a 'main' entry point for a program?
+
+class CFGBuilderContext:  # is it required to have a 'main' entry point for a program?
     def __init__(self):
         self.cfgs = {}
         self.current_cfg = None
@@ -55,7 +83,7 @@ class CFGBuilderContext: # is it required to have a 'main' entry point for a pro
 
     def build(self, node: AssemblyNode):
         return self(node)
-    
+
     def __call__(self, node: AssemblyNode, break_block_id: str = None):
         match node:
             case Literal(value):
@@ -65,20 +93,26 @@ class CFGBuilderContext: # is it required to have a 'main' entry point for a pro
             case Repack(val):
                 self.current_cfg.current_block.add_statement(("repack", val))
             case Resize(buffer, new_size):
-                self.current_cfg.current_block.add_statement(("resize", buffer, new_size))
+                self.current_cfg.current_block.add_statement(
+                    ("resize", buffer, new_size)
+                )
             case Variable(name, type):
                 self.current_cfg.current_block.add_statement(("variable", name, type))
             case GetAttr(obj, attr):
                 self.current_cfg.current_block.add_statement(("getattr", obj, attr))
             case SetAttr(obj, attr, value):
-                self.current_cfg.current_block.add_statement(("setattr", obj, attr, value))
+                self.current_cfg.current_block.add_statement(
+                    ("setattr", obj, attr, value)
+                )
             case Call(Literal(_) as lit, args):
                 # TODO: handle it as a statement for now (?)
                 ...
             case Load(buffer, index):
                 self.current_cfg.current_block.add_statement(("load", buffer, index))
             case Store(buffer, index, value):
-                self.current_cfg.current_block.add_statement(("store", buffer, index, value))
+                self.current_cfg.current_block.add_statement(
+                    ("store", buffer, index, value)
+                )
             case Length(buffer):
                 self.current_cfg.current_block.add_statement(("length", buffer))
             case Slot(name, type):
@@ -97,12 +131,14 @@ class CFGBuilderContext: # is it required to have a 'main' entry point for a pro
                 then_block = self.current_cfg.new_block()
                 self.current_cfg.current_block = then_block
                 self(body, break_block_id)
-                
+
                 after_block = self.current_cfg.new_block()
                 cond_block.add_successor(then_block.id, self.current_cfg.blocks)
                 cond_block.add_successor(after_block.id, self.current_cfg.blocks)
 
-                self.current_cfg.current_block.add_successor(after_block.id, self.current_cfg.blocks)
+                self.current_cfg.current_block.add_successor(
+                    after_block.id, self.current_cfg.blocks
+                )
                 self.current_cfg.current_block = after_block
             case IfElse(cond, body, else_body):
                 cond_block = self.current_cfg.current_block
@@ -111,17 +147,21 @@ class CFGBuilderContext: # is it required to have a 'main' entry point for a pro
                 then_block = self.current_cfg.new_block()
                 self.current_cfg.current_block = then_block
                 self(body, break_block_id)
-                
+
                 after_block = self.current_cfg.new_block()
                 cond_block.add_successor(then_block.id, self.current_cfg.blocks)
                 cond_block.add_successor(after_block.id, self.current_cfg.blocks)
-                self.current_cfg.current_block.add_successor(after_block.id, self.current_cfg.blocks)
+                self.current_cfg.current_block.add_successor(
+                    after_block.id, self.current_cfg.blocks
+                )
 
                 else_block = self.current_cfg.new_block()
                 self.current_cfg.current_block = else_block
                 self(else_body, break_block_id)
 
-                self.current_cfg.current_block.add_successor(after_block.id, self.current_cfg.blocks)
+                self.current_cfg.current_block.add_successor(
+                    after_block.id, self.current_cfg.blocks
+                )
                 self.current_cfg.current_block = after_block
             case WhileLoop(cond, body):
                 cond_block = self.current_cfg.current_block
@@ -129,14 +169,16 @@ class CFGBuilderContext: # is it required to have a 'main' entry point for a pro
 
                 body_block = self.current_cfg.new_block()
                 after_block = self.current_cfg.new_block()
-                
+
                 cond_block.add_successor(body_block.id, self.current_cfg.blocks)
                 cond_block.add_successor(after_block.id, self.current_cfg.blocks)
-                
+
                 self.current_cfg.current_block = body_block
                 self(body, after_block.id)
-                
-                self.current_cfg.current_block.add_successor(cond_block.id, self.current_cfg.blocks)
+
+                self.current_cfg.current_block.add_successor(
+                    cond_block.id, self.current_cfg.blocks
+                )
                 self.current_cfg.current_block = after_block
             case ForLoop(var, start, end, body):
                 init_block = self.current_cfg.current_block
@@ -148,7 +190,7 @@ class CFGBuilderContext: # is it required to have a 'main' entry point for a pro
 
                 body_block = self.current_cfg.new_block()
                 cond_block.add_successor(body_block, self.current_cfg.blocks)
-                
+
                 after_block = self.current_cfg.new_block()
                 cond_block.add_successor(after_block, self.current_cfg.blocks)
 
@@ -156,54 +198,68 @@ class CFGBuilderContext: # is it required to have a 'main' entry point for a pro
                 self(body_block, after_block.id)
 
                 self.current_cfg.current_block.add_statement(("for_inc", var))
-                self.current_cfg.current_block.add_successor(cond_block.id, self.current_cfg.blocks)
+                self.current_cfg.current_block.add_successor(
+                    cond_block.id, self.current_cfg.blocks
+                )
 
                 self.current_cfg.current_block = after_block
             case Return(value):
                 self.current_cfg.current_block.add_statement(("return", value))
-                
+
                 # when Return is met, make a connection to the EXIT block of function (cfg)
-                self.current_cfg.current_block.add_successor(self.current_cfg.exit_block.id, self.current_cfg.blocks)
-                
+                self.current_cfg.current_block.add_successor(
+                    self.current_cfg.exit_block.id, self.current_cfg.blocks
+                )
+
                 # create a block where we going to store all unreachable statements
                 unreachable_block = self.current_cfg.new_block()
                 self.current_cfg.current_block = unreachable_block
             case Break():
-                self.current_cfg.current_block.add_statement(("break"))
+                self.current_cfg.current_block.add_statement("break")
 
                 # when Break is met, make a connection to the AFTER block of ForLoop/WhileLoop
-                self.current_cfg.current_block.add_successor(break_block_id, self.current_cfg.blocks)
+                self.current_cfg.current_block.add_successor(
+                    break_block_id, self.current_cfg.blocks
+                )
 
                 # create a block where we going to store all unreachable statements
                 unreachable_block = self.current_cfg.new_block()
                 self.current_cfg.current_block = unreachable_block
-            case BufferLoop(buf, var, body):
+            case BufferLoop(_buf, var, body):
                 # TODO: 1) implement BufferLoop printing and then implement this
                 # TODO: 2) implement BufferLoop here
                 raise NotImplementedError(node)
             case Function(Variable(_, _), args, body):
                 for arg in args:
                     match arg:
-                        case Variable(name, t):
-                            self.current_cfg.current_block.add_statement(("func_arg"), name)
+                        case Variable(name, type):
+                            self.current_cfg.current_block.add_statement(
+                                ("func_arg"), name, type
+                            )
                         case _:
-                            raise NotImplementedError(f"Unrecognized argument type: {arg}")
-                
+                            raise NotImplementedError(
+                                f"Unrecognized argument type: {arg}"
+                            )
+
                 self(body)
             case Module(funcs):
                 for func in funcs:
                     if not isinstance(func, Function):
-                        raise NotImplementedError(f"Unrecognized function type: {type(func)}")
-                    
+                        raise NotImplementedError(
+                            f"Unrecognized function type: {type(func)}"
+                        )
+
                     self.current_cfg = self.new_cfg(func.name.name)
                     self(func)
                     end_func_block = self.current_cfg.new_block()
-                    self.current_cfg.current_block.add_successor(end_func_block, self.current_cfg.blocks)
+                    self.current_cfg.current_block.add_successor(
+                        end_func_block, self.current_cfg.blocks
+                    )
                     self.current_cfg.current_block = end_func_block
                 return None
             case node:
                 raise NotImplementedError(node)
-        
+
         return self.cfgs
 
 
@@ -224,6 +280,6 @@ cfgs: {
 }
 """
 
+
 # TODO: CFG Printer
-class CFGPrinter:
-    ...
+class CFGPrinter: ...
